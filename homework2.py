b0183ac46011b56c457a3e3c3044c9eb5ad165ff
VladProkopenko1 <nonprokopenko@gmail.com>
2024-01-18 17:31:23 +0200

    new name task 2

--- homework2.py
+++ homework2.py
@@ -60,4 +60,45 @@ if operation == '**':
 if operation == 'Exit':
     print('program completed')
 
+# Task 2 with Cycle while True
 
+while True:
+    operation = input("Enter operation 2.0: '+', '-', '*', '/', '//', '%' '**', 'Exit': ")
+
+    if operation == 'Exit':
+        print('Program completed.')
+        break
+
+    Num_1 = float(input('Enter first numb:'))
+    Num_2 = float(input('Enter second numb:'))
+
+    if operation == '+':
+        result_2 = Num_1 + Num_2
+
+    elif operation == '-':
+        result_2 = Num_1 - Num_2
+
+    elif operation == '*':
+        result_2 = Num_1 * Num_2
+
+    elif operation == '/':
+        if Num_2 != 0:
+            result_2 = Num_1 / Num_2
+        else:
+            print("Error: Division by zero")
+            continue
+
+    elif operation == '//':
+        result_2 = Num_1 // Num_2
+
+    elif operation == '%':
+        result_2 = Num_1 % Num_2
+
+    elif operation == '**':
+        result_2 = Num_1 ** Num_2
+
+    else:
+        print("Invalid operation. Please enter a valid operation.")
+        continue
+
+    print("Result:", result_2)
